0478b37cd04d2957bc40673641fbd855458c4600
Kingman <107763929+KingmanT@users.noreply.github.com>
2022-11-14 20:21:24 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from application import app, greet
 def test_quick():
   a = "jeff"
   greeting = greet(a)
-  assert greeting == "Hi jeff "
+  assert greeting == "Hi jeff"
 
 # def test_home_page():
 #     response = app.test_client().get('/')
